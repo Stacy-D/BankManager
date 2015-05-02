6a67679bb9e9fe77776e1f5623c553803bc9582f
Natalia <nataliakiselyk@gmail.com>
2015-05-02 21:08:11 +0300

    some changes

--- src/bankManager/Client.java
+++ src/bankManager/Client.java
@@ -92,89 +92,6 @@ public class Client {
 	}
 	
 	
-	public void createDatastore(Client aClient) {
-		 try {
-	            Class.forName("org.sqlite.JDBC");
-	            connection = DriverManager.getConnection("jdbc:sqlite: Bank");
-	            PreparedStatement st = connection.prepareStatement("create table if not exists 'Bank' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text, 'clientId' INTEGER,'password' INTEGER, 'money' INTEGER,'cardNumber' INTEGER);");
-	            int result = st.executeUpdate();
-	        } catch (ClassNotFoundException e) {
-	            System.out.println(" JDBC");
-	            e.printStackTrace();
-	            System.exit(0);
-	        } catch (SQLException e) {
-	            System.out.println("Wrong SQL");
-	            e.printStackTrace();
-	        }    
-	}
-	
-	public void addClientToDatastore(Client aClient) {
-		if (aClient != null && !isClientInDatastore()) {
-			try{
-	        	String name = aClient.getName();
-	        	String password = aClient.getPassword();
-	        	String clientId = aClient.getId();
-	        	int cardNumber = aClient.getCardNumber();
-	        	int money = aClient.getMoneyOnBanlAccount();
-	          
-	        	PreparedStatement statement = connection.prepareStatement("INSERT INTO Bank(name, password, clientId, money, cardNumber) VALUES (?,?,?,?,?)");
-	            statement.setString(1, name);
-	            statement.setString(2, password);
-	            statement.setString(3, clientId);
-	            statement.setInt(4, money);
-	            statement.setInt(5, cardNumber);
-
-	            int result = statement.executeUpdate();
-
-	            statement.close();
-	        }catch (SQLException e) {
-	            System.out.println("Wrong SQL");
-	            e.printStackTrace();
-	        }
-		}
-	}
-	
-	public boolean isClientInDatastore() {
-		Statement statement = null;
-    	try {
-    		statement = connection.createStatement();
-    		ResultSet resSet = statement.executeQuery("SELECT * FROM Bank;");
-    		while (resSet.next()) {
-    			int id = resSet.getInt("clientId");
-    			String name = resSet.getString("name");
-    			System.out.println("clientId: " + id);
-    			System.out.println("Name: " + name);
-    			System.out.println("");
-    		}
-    		resSet.close();
-    		statement.close();
-    	} catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	System.out.println("Operation done successfully");
-		return true;
-	}
-	
-	public void removeFromDatastore(int clientId) {
-		PreparedStatement statement = null;
-    	try {
-    		statement = connection.prepareStatement("DELETE FROM Bank WHERE clientId = ?");
-    		statement.setInt(3, clientId);
-    		statement.executeUpdate();
-    		moneyOnBanlAccount = 0;
-    		System.out.println("The client with id " + clientId + " was deleted");
-    	} catch (SQLException e) {
-    		e.printStackTrace();
-    	} finally {
-    		if (statement != null) {
-    			try {
-    				statement.close();
-    			} catch (SQLException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    	}
-	}
 
 	/**
 	 * check if can do a transaction
@@ -197,8 +114,9 @@ public class Client {
 						break;
 					}
 				}
-			}
+			} 
 			if (sum <= MAX_DAY_LIMIT) {
+				moneyLimit = (int) (MAX_DAY_LIMIT - sum);
 				return true;
 			}
 		}
@@ -259,6 +177,14 @@ public class Client {
 		return (moneyToAdd+moneyOnBanlAccount);
 	}
 
+	/**
+	 * return today limit
+	 */
+	
+	public int todayLimit() {
+		return moneyLimit;
+	}
+	
 	@Override
 	public String toString() {
 		return "Client [name=" + name + ", id=" + id + ", password=" + password
